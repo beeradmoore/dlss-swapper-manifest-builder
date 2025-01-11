5cb9ee893e985b7090b4e3851a9df2507e2c5d65
Brad <brad@4pi.com.au>
2025-01-12 09:01:21 +1100

    Added XeLL and XeSS FG

--- src/DLSS Swapper Manifest Builder/Processors/XeSSFGProcessor.cs
+++ src/DLSS Swapper Manifest Builder/Processors/XeSSFGProcessor.cs
@@ -1,21 +1,21 @@
-﻿using System.Diagnostics;
-using System.IO.Compression;
-using System.Text.Json;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace DLSS_Swapper_Manifest_Builder.Processors;
 
-
-public class XeSSProcessor : DLLProcessor
+internal class XeSSFGProcessor : DLLProcessor
 {
-    public override string NamePath => "xess";
-    public override string ExpectedDLLName => "libxess.dll";
+    public override string NamePath => "xess_fg";
+    public override string ExpectedDLLName => "libxess_fg.dll";
     public override string[] ValidFileDescriptions => new string[]
     {
-        "XeSS SDK"
+        "XeSS Frame Generation SDK"
     };
     public override string[] ExpectedPrefix => new string[]
     {
-        "bin/",
         "/",
     };
     public override string[] ExpectedDevPrefix => new string[]
@@ -25,7 +25,6 @@ public class XeSSProcessor : DLLProcessor
     public override Dictionary<string, string> CustomAdditionalLabels => new Dictionary<string, string>();
     public override Dictionary<string, string> DllSource => new Dictionary<string, string>()
     {
-        { "C9FB6BB716A087FD537790BEBE710D4C", "Forza Horizon 5" }, // v1.1.0.18
-        { "367BE8343B288372FF8523438F53B62D", "Marvel Rivals" }, // v2.0.0.18
+        { "BF976A1C2E466B1BE6FC4B17D76B50D5", "Marvel Rivals" }, // v1.0.0.16
     };
 }
--- src/DLSS Swapper Manifest Builder/Program.cs
+++ src/DLSS Swapper Manifest Builder/Program.cs
@@ -55,9 +55,11 @@ if (manifest == null)
 var dlssProcessor = new DLSSProcessor();
 var dlssgProcessor = new DLSSGProcessor();
 var dlssdProcessor = new DLSSDProcessor();
-var xessProcessor = new XeSSProcessor();
 var fsr31dx12Processor = new FSR31DX12Processor();
 var fsr31vkProcessor = new FSR31VKProcessor();
+var xessProcessor = new XeSSProcessor();
+var xellProcessor = new XeLLProcessor();
+var xessfgProcessor = new XeSSFGProcessor();
 
 await dlssProcessor.DownloadExistingRecordsAsync(manifest.DLSS);
 manifest.DLSS = dlssProcessor.ProcessLocalFiles(manifest.DLSS);
@@ -68,16 +70,22 @@ manifest.DLSS_G = dlssgProcessor.ProcessLocalFiles(manifest.DLSS_G);
 await dlssdProcessor.DownloadExistingRecordsAsync(manifest.DLSS_D);
 manifest.DLSS_D = dlssdProcessor.ProcessLocalFiles(manifest.DLSS_D);
 
-await xessProcessor.DownloadExistingRecordsAsync(manifest.XeSS);
-manifest.XeSS = xessProcessor.ProcessLocalFiles(manifest.XeSS);
-
 await fsr31dx12Processor.DownloadExistingRecordsAsync(manifest.FSR_31_DX12);
 manifest.FSR_31_DX12 = fsr31dx12Processor.ProcessLocalFiles(manifest.FSR_31_DX12);
 
 await fsr31vkProcessor.DownloadExistingRecordsAsync(manifest.FSR_31_VK);
 manifest.FSR_31_VK = fsr31vkProcessor.ProcessLocalFiles(manifest.FSR_31_VK);
 
+await xessProcessor.DownloadExistingRecordsAsync(manifest.XeSS);
+manifest.XeSS = xessProcessor.ProcessLocalFiles(manifest.XeSS);
+
+await xellProcessor.DownloadExistingRecordsAsync(manifest.XeLL);
+manifest.XeLL = xellProcessor.ProcessLocalFiles(manifest.XeLL);
+
+await xessfgProcessor.DownloadExistingRecordsAsync(manifest.XeSS_FG);
+manifest.XeSS_FG = xessfgProcessor.ProcessLocalFiles(manifest.XeSS_FG);
 var manifestJson = JsonSerializer.Serialize(manifest, new JsonSerializerOptions() { WriteIndented = true });
+
 File.WriteAllText(DLLProcessor.OutputManifestPath, manifestJson);
 
 // Copy to root of the repo
