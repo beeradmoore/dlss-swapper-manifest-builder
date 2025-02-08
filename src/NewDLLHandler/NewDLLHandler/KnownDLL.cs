51c65e6e0e083d5ffababbb9f79fc5a3515cf385
Brad <brad@4pi.com.au>
2025-02-08 16:40:25 +1100

    Changed json names of KnownDLL

--- src/NewDLLHandler/NewDLLHandler/KnownDLL.cs
+++ src/NewDLLHandler/NewDLLHandler/KnownDLL.cs
@@ -2,15 +2,23 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.Json.Serialization;
 using System.Threading.Tasks;
 
 namespace NewDLLHandler;
 
 internal class KnownDLL : IEquatable<KnownDLL>
 {
+    [JsonPropertyName("dll_type")]
     public string DLLType { get; set; } = string.Empty;
+
+    [JsonPropertyName("version")]
     public string Version { get; set; } = string.Empty;
+
+    [JsonPropertyName("hash")]
     public string Hash { get; set; } = string.Empty;
+
+    [JsonPropertyName("sources")]
     public Dictionary<string, List<string>> Sources { get; set; } = new Dictionary<string, List<string>>();
 
     public bool Equals(KnownDLL? other)
